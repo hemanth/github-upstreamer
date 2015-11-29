3a18dd289436467263e5d32ea5b9cb756256ce54
Hemanth.HM <hemanth.hm@gmail.com>
2015-11-29 10:33:21 +0530

    Take-1 trying to make travis happy.

--- test.js
+++ test.js
@@ -7,10 +7,14 @@ var join = require('path').join;
 execSync('git clone https://github.com/hemanth/maybe-hugs.git fixture');
 
 describe("Test github-upstreamer", function(done) {
+
 	it('should work on a non-fork repo', function (done) {
+		var cwd = process.cwd();
+		chdir('/');
 		githubUpstreamer((err, res) => {
 			assert.equal(!err, true, "No error");
 			assert.equal(res, null, "It's not a fork");
+			chdir(cwd);
 			done();
 		});
 	});
@@ -20,13 +24,10 @@ describe("Test github-upstreamer", function(done) {
 		githubUpstreamer('./fixture',(err, res) => {
 			assert.equal(!err, true, "No error");
 			assert.equal(res, '', "Must have added the upstream");
-			var cwd = process.cwd();
-		    chdir(join(__dirname, 'fixture'));
 			assert.equal(
-				/zkat/.test(execSync('git config --get remote.upstream.url', {encoding: 'utf-8'})),
+				/zkat/.test(execSync(`cd ${__dirname}/fixture && git config --get remote.upstream.url`, {encoding: 'utf-8'})),
 				true,
 				'Must have set the upstream to original author');
-			chdir(cwd);
 			done();
 		});
 	});
@@ -38,13 +39,10 @@ describe("Test github-upstreamer", function(done) {
 			console.log(err,res);
 			assert.equal(!err, true, "No error");
 			assert.equal(res, '', "Must have added the upstream");
-			var cwd = process.cwd();
-		    chdir(join(__dirname, 'fixture'));
 		    assert.equal(
-		    	/zkat/.test(execSync('git config --get remote.upstream2.url', {encoding: 'utf-8'})),
+		    	/zkat/.test(execSync(`cd ${__dirname}/fixture && git config --get remote.upstream2.url`, {encoding: 'utf-8'})),
 		    	true,
 		    	'Must have set the upstream to original author');
-			chdir(cwd);
 			done();
 		});
 	});
