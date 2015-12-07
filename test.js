ad5060d159660037176e147d61961b47f0c0c5f9
Hemanth.HM <hemanth.hm@gmail.com>
2015-12-07 13:51:40 +0530

    Travis will be happy.

--- test.js
+++ test.js
@@ -6,23 +6,28 @@ var join = require('path').join;
 
 describe("Test github-upstreamer", function(done) {
 	beforeEach(function(done) {
+		this.timeout(3500);
 		execSync('git clone https://github.com/hemanth/maybe-hugs.git fixture');
 		done();
 	});
 
 	afterEach(function(done) {
+		this.timeout(3500);
 		execSync('rm -rf fixture');
+		done();
 	});
 
 	it('should work on a non-fork repo', function (done) {
+		this.timeout(3500);
 		var cwd = process.cwd();
 		chdir('/');
 		githubUpstreamer((err, res) => {
 			assert.equal(!err, true, "No error");
 			assert.equal(res, null, "It's not a fork");
 			chdir(cwd);
-			done();
 		});
+		chdir(cwd);
+		done();
 	});
 
 	it('sould set the upstream to the orginal author', function(done) {
@@ -30,27 +35,27 @@ describe("Test github-upstreamer", function(done) {
 		githubUpstreamer('./fixture',(err, res) => {
 			assert.equal(!err, true, "No error");
 			assert.equal(res, '', "Must have added the upstream");
+			console.log(`${__dirname}/fixture`);
 			assert.equal(
 				/zkat/.test(execSync(`cd ${__dirname}/fixture && git config --get remote.upstream.url`, {encoding: 'utf-8'})),
 				true,
 				'Must have set the upstream to original author');
-			done();
 		});
+		done();
 	});
 
 
 	it('sould set the upstream to the orginal author for the given name', function(done){
 		this.timeout(3500);
 		githubUpstreamer("./fixture", "upstream2", (err, res) => {
-			console.log(err,res);
 			assert.equal(!err, true, "No error");
 			assert.equal(res, '', "Must have added the upstream");
 		    assert.equal(
 		    	/zkat/.test(execSync(`cd ${__dirname}/fixture && git config --get remote.upstream2.url`, {encoding: 'utf-8'})),
 		    	true,
 		    	'Must have set the upstream to original author');
-			done();
 		});
+	    done();
 	});
 });
 
